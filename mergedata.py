86c4d86e132473c6a21e883ef615e4337a7033f4
AnonimniyGeniy <68807643+AnonimniyGeniy@users.noreply.github.com>
2022-10-09 03:24:26 +0300

    merged_data commit

--- mergedata.py
+++ mergedata.py
@@ -2,18 +2,41 @@ import json
 
 import pandas as pd
 
-fb = pd.read_csv("forbes_data.csv")
-fbuh = pd.read_csv("buh_data.csv")
-frbk = pd.read_csv("forbes_data.csv")
+fb = pd.read_csv("forbes_data.csv") #1
+fbuh = pd.read_csv("buh_data.csv") #2
+feconomics = pd.read_csv("rbk_economics.csv") #1
+ffinances = pd.read_csv("rbk_finances.csv") #1
+fpolitics = pd.read_csv("rbk_politics.csv") #3
+ftrash = pd.read_csv("rbk_technology_and_media.csv") #3
 
 newdata = pd.DataFrame()
 newdata["link"] = ""
 newdata["title"] = ""
 newdata["text"] = ""
 newdata["category"] = ""
-for i in fb:
-    newdata["link"] = i["link"]
-    newdata["text"] = i["text"]
-    newdata["title"] = i["title"]
-    newdata["cat"] = 1
-newdata.to_csv(open("merged_data"))
+sp = []
+for i in range(len(fb)):
+    #print(fb['link'][i])
+    sp.append([fb['link'][i], fb['text'][i], fb['title'][i], 1])
+
+for i in range(len(fbuh)):
+    #print(fb['link'][i])
+    sp.append([fbuh['link'][i], fbuh['text'][i], fbuh['title'][i], 2])
+
+for i in range(len(feconomics)):
+    #print(fb['link'][i])
+    sp.append([feconomics['href'][i], feconomics['text'][i], feconomics['title'][i], 1])
+
+for i in range(len(ffinances)):
+    #print(fb['link'][i])
+    sp.append([ffinances['href'][i], ffinances['text'][i], ffinances['title'][i], 1])
+
+for i in range(len(fpolitics)):
+    #print(fb['link'][i])
+    sp.append([fpolitics['href'][i], fpolitics['text'][i], fpolitics['title'][i], 3])
+for i in range(len(ftrash)):
+    #print(fb['link'][i])
+    sp.append([ftrash['href'][i], ftrash['text'][i], ftrash['title'][i], 3])
+
+newdata = pd.DataFrame(data=sp)
+newdata.to_csv("merged_data.csv")
