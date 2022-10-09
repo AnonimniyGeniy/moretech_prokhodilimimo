cf491ec6fa0823ada052b838c1bb8a49f9029e88
Zakharov_Kirill_Sergeevich <kil4gen@gmail.com>
2022-10-09 07:29:35 +0300

    aslkdjsdjnjf

---
+++ parse.sh
@@ -0,0 +1,9 @@
+cd parsers
+while [[ true ]]
+do
+D:/Anaconda/envs/PyCharnEnv/python.exe parse_rbk.py
+D:/Anaconda/envs/PyCharnEnv/python.exe parse_buhgalter.py
+D:/Anaconda/envs/PyCharnEnv/python.exe parse_forbes.py
+D:/Anaconda/envs/PyCharnEnv/python.exe mergedata.py
+sleep 18000
+done
